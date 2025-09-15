13ec2140385e7af58a62300c935825ac23aa7661
nngruters <nngruters@gmail.com>
2025-09-15 07:47:15 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.compassHeading())
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
